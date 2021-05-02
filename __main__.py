a34c937bf9cf31805673f633d53e8dee4ad777d7
edieguez <dieguezemmanuel@gmail.com>
2021-05-02 15:42:05 -0500

    Added basic error handler

--- __main__.py
+++ __main__.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-from os import environ, remove
+import os
 
 from telegram import Update, ChatAction
 from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext
@@ -12,7 +12,7 @@ def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Welcome to my velvet room')
 
 
-def hello(update: Update, context: CallbackContext) -> None:
+def download_video(update: Update, context: CallbackContext) -> None:
     update.message.reply_chat_action(ChatAction.RECORD_AUDIO)
     audio_filename = download_audio(update.message.text)
     audio_file = open(audio_filename, 'rb')
@@ -20,13 +20,18 @@ def hello(update: Update, context: CallbackContext) -> None:
     update.message.reply_chat_action(ChatAction.UPLOAD_AUDIO)
     update.message.reply_audio(audio_file, quote=True, timeout=180)
 
-    remove(audio_file.name)
+    os.remove(audio_file.name)
 
 
-API_BOT_TOKEN = environ['API_BOT_TOKEN']
+def error_handler(update: Update, context: CallbackContext) -> None:
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Unexpected error")
+
+
+API_BOT_TOKEN = os.environ['API_BOT_TOKEN']
 updater = Updater(API_BOT_TOKEN)
 updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(MessageHandler((Filters.text), hello))
+updater.dispatcher.add_handler(MessageHandler((Filters.text), download_video))
+updater.dispatcher.add_error_handler(error_handler)
 
 updater.start_polling()
 updater.idle()
